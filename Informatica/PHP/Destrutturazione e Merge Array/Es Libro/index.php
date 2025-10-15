2ed5a217bb610b36c550f116943e7fff93a046f6
alenurdewa <alessandronurdewa@gmail.com>
2025-10-15 12:58:42 +0200

    Esercizio Libro php

---
+++ Informatica/PHP/Destrutturazione e Merge Array/Es Libro/index.php
@@ -0,0 +1,12 @@
+<?php
+
+function destruttura ($libro){
+    ["titolo" => $t, "autore" => $aut, "prezzo"=> $p, "anno" => $anno, "copie" => $copie] = $libro;
+
+}
+
+
+$libro = ["titolo" => "I gatti", "autore" => "Alessandro Berti", "prezzo"=> "69", "anno" => "2024", "copie" => "100"]
+
+
+?>
